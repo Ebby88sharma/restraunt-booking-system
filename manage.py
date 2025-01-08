7d40a1cfaf159149f9075797a237f20c4a13ea9d
Abhinav Sharma <ab88sharma@gmail.com>
2025-01-08 10:51:51 +0000

    env file updated for deployement

--- manage.py
+++ manage.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booking_system.settings')
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booking_system.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
